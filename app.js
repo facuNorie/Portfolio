24a9789d9acdc7fb1806bdadf2bcff43a4f5d37a
facuNorie <noriegafacundo08@gmail.com>
2021-02-20 18:34:31 -0300

    Inicializando el proyecto, casi que terminado el main

---
+++ app.js
@@ -0,0 +1,6 @@
+const sectionsPage = new fullpage(`#fullpage`,{
+    navigation: true,
+    menu: `menu`,
+    anchors: [`home`,`about`,`skills`,`portfolio`,`contact`],
+    navigationTooltips: [`Home`,`About`,`Skills`,`Portfolio`,`Contact`]
+});
