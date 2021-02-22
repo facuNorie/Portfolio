df4de8a4fd827912d56f21f4a8c1f4803da62792
facuNorie <noriegafacundo08@gmail.com>
2021-02-22 15:56:00 -0300

    agregando skills, cambiando paleta de colores de la pagina

--- app.js
+++ app.js
@@ -4,3 +4,44 @@ const sectionsPage = new fullpage(`#fullpage`,{
     anchors: [`home`,`about`,`skills`,`portfolio`,`contact`],
     navigationTooltips: [`Home`,`About`,`Skills`,`Portfolio`,`Contact`]
 });
+
+// Create Icons
+
+const icons = [{
+    name: `HTML`,
+    url: `./media/html5-1.svg`
+},
+{
+    name: `CSS`,
+    url: `./media/css-5.png`
+},
+{
+    name: `SASS`,
+    url: `./media/sass-1.svg`
+},
+{
+    name: `JAVASCRIPT`,
+    url: `./media/logo-javascript.svg`
+},
+{
+    name: `GIT`,
+    url: `./media/git-icon.svg`
+},
+{
+    name: `GITHUB`,
+    url: `./media/github-icon-1.svg`
+},
+];
+
+for (icon of icons) {
+    let nameIcon = document.createElement("P");
+    nameIcon.textContent = icon.name;
+    let imgIcon = document.createElement("IMG");
+    imgIcon.src = icon.url;
+    imgIcon.classList.add(`imgIcon-${icon.name}`);
+    let divIcons = document.createElement("DIV");
+    divIcons.classList.add("icons");
+    divIcons.appendChild(nameIcon);
+    divIcons.appendChild(imgIcon);
+    document.querySelector(".divIconsSkills").appendChild(divIcons);
+}
